7d348b61602c1e1c8b71c0ec6c04245e27d99104
krishna09062001 <122146749+krishna09062001@users.noreply.github.com>
2023-04-15 22:08:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(printIfDeveloperMap)
+  function printIfDeveloperMap(employee){
+    if(employee.profession === "developer"){
+      console.log(employee)
+    }
+  }
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+   arr.forEach(printIfDeveloperForEach)
+
+  function printIfDeveloperForEach(employee){
+    if(employee.profession === "developer"){
+      console.log(employee)
+    }
+  }
 }
 
 function addData() {
   //Write your code here, just console.log
+  let objToBeAdded={id:4,name:"susan",age:"20",profession:"intern"}
+  arr.push(objToBeAdded)
+  console.log(arr)
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr=arr.filter(person=> person.profession !== "admin")
+  
+  console.log(arr)
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arrayToBeConcatenated = [
+    { id: 5, name: "excal", age: "30", profession: "swordsman" },
+    { id: 6, name: "mag", age: "31", profession: "mage" },
+    { id: 7, name: "volt", age: "32", profession: "needforspeeeeeed" },
+  ];
+
+  arr=arr.concat(arrayToBeConcatenated)
+
+  console.log(arr)
 }
